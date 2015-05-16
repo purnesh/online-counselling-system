d2f2d45b06b49535bab53ee55ee7e402bae76132
Purnesh Tripathi <purnesh.xyz@gmail.com>
2015-05-17 02:39:30 +0530

    Commit 07 - Script Documented
    The JavaScript controller defined till now were properly documented for ease in future use.

--- front-end/content/user/js/script.js
+++ front-end/content/user/js/script.js
@@ -4,11 +4,26 @@
 
 var ocs = angular.module('cotocs', []);
 
+/*
+ ======================================
+ headerDisplayHandler Controller BEGINS
+ This function will be responsible for defining all the background variables for header
+ */
 ocs.controller('headerDisplayHandler', function($scope){
     $scope.displayData = {title: "Online Counselling System",
                             author: "Purnesh Tripathi"};
 });
+/*
+ headerDisplayHandler Controller ENDS
+ This function will be responsible for defining all the background variables for header
+ ======================================
+ */
 
+/*
+ ======================================
+ navigationBarHandler Controller BEGINS
+ This function will be responsible for defining all functions of the Navigation Bar
+ */
 ocs.controller('navigationBarHandler', function($scope) {
     $scope.projectTitle = "Online Counselling System";
 
@@ -19,5 +34,8 @@ ocs.controller('navigationBarHandler', function($scope) {
                         href: "rules.html",
                         id: "rules-tab-navbar"}];
 });
-
-
+/*
+ navigationBarHandler Controller ENDS
+ This function will be responsible for defining all functions of the Navigation Bar
+ ======================================
+ */
