ad72aef9a283a1799e59a4d652a9ede48ae20cd8
Purnesh Tripathi <purnesh.xyz@gmail.com>
2015-05-16 14:28:44 +0530

    Commit 05
    Bootstrap Basic Template has been successfully integrated for all non-dashboard purposes.

--- front-end/content/user/js/jsTests/script_test.js
+++ front-end/content/user/js/jsTests/script_test.js
@@ -1,24 +1,57 @@
 /**
  * Created by hokage on 16/5/15.
  */
+describe('Home Page Tests', function () {
 
-describe('headerDisplayController function', function(){
-    var $scope;
-    beforeEach(module('cotocs'));
+    describe('headerDisplayHandler Controller', function(){
+        var $scope;
+        beforeEach(module('cotocs'));
 
-    beforeEach(inject(function (_$controller_) {
-        $controller = _$controller_;
-    }));
+        beforeEach(inject(function (_$controller_) {
+            $controller = _$controller_;
+        }));
 
-    beforeEach(inject(function($rootScope) {
-        $scope = $rootScope.$new();
-        controller = $controller('headerDisplayController', {$scope: $scope});
-    }));
+        beforeEach(inject(function($rootScope) {
+            $scope = $rootScope.$new();
+            controller = $controller('headerDisplayHandler', {$scope: $scope});
+        }));
+
+        it('should Exist', function () {
+            expect(controller).not.toBeNull();
+        });
+
+        it('should set Author Name for the webpage', function (){
+            expect($scope.displayData.author).toBe('Purnesh Tripathi');
+        });
+
+        it('should set Title for the webpage', function (){
+            expect($scope.displayData.title).not.toBeNull();
+        });
+    });
+
+    describe('navigationBarHandler Controller', function(){
+        var $scope;
+        beforeEach(module('cotocs'));
+
+        beforeEach(inject(function (_$controller_) {
+            $controller = _$controller_;
+        }));
+
+        beforeEach(inject(function($rootScope) {
+            $scope = $rootScope.$new();
+            controller = $controller('navigationBarHandler', {$scope: $scope});
+        }));
+
+        it('should Exist', function () {
+            expect(controller).not.toBeNull();
+        });
+
+        it('should contain at least one menu value', function()
+        {
+            expect($scope.entries.length).not.toBeNull();
+        });
 
 
-
-    it('should set title for the webpage', function (){
-        expect($scope.displayData.author).toBe('Purnesh Tripathi');
     });
 
 });
--- front-end/content/user/js/script.js
+++ front-end/content/user/js/script.js
@@ -4,11 +4,20 @@
 
 var ocs = angular.module('cotocs', []);
 
-ocs.controller('headerDisplayController', function($scope){
+ocs.controller('headerDisplayHandler', function($scope){
     $scope.displayData = {title: "Online Counselling System",
                             author: "Purnesh Tripathi"};
-
 });
 
+ocs.controller('navigationBarHandler', function($scope) {
+    $scope.projectTitle = "Online Counselling System";
+
+    $scope.entries = [{name: "Home",
+                        href: "index.html",
+                        id: "home-tab-navbar"},
+                    {name: "Rules",
+                        href: "rules.html",
+                        id: "rules-tab-navbar"}];
+});
 
 
