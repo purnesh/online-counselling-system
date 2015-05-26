467f9a3f266fa2e006bfbbe97231d9c0eb98f5eb
Purnesh Tripathi <purnesh.xyz@gmail.com>
2015-05-26 09:55:49 +0530

    Commit 19 - JWT library tested and fully functional. Time for RESTful API creation
    CORS Request, JSON Web Tokens, API for authentication, feedback and reception at AngularJS has been successfully tested.

--- back-end/application/controllers/Authenticate.php
+++ back-end/application/controllers/Authenticate.php
@@ -10,9 +10,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Authenticate extends CI_Controller {
 
+    public function __construct($config = 'rest')
+    {
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        parent::__construct();
+    }
+
     public function user($type, $username, $password){
-        $password = $this->encryption->encrypt($password);
-        echo $type.$username.$password;
+        $this->load->library('JWT');
+        $password = $this->jwt->encode('asd', 'Purnesh');
+        echo $password;
     }
 }
 
